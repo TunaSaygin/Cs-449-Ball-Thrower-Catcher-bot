ad2a917c13f9b0a4c19ebad06c471d3a58d7aaa2
Muhammad Shayan Usman <mshayanalwaha@gmail.com>
2025-01-04 22:27:07 +0300

    RRT MP in tg.py + added constraints in post_grasp and throw_object

--- rrt_move.py
+++ rrt_move.py
@@ -6,7 +6,41 @@ from catcher_robot import VisionCatcherRobot
 max_trials = 10
 
 
-def move_robot_closer(thrower_fname, catcher_fname, waypoints, C: ry.Config, isRender:bool):
+def calc_target_pos(thrower_pos, catcher_pos, clearance_dist=2.5):
+    """
+    Function to calculate the target position for the thrower robot.
+
+    Args:
+    - thrower_pos: np.array, position of the thrower robot
+    - catcher_pos: np.array, position of the catcher robot
+    - clearance_dist: float, distance to keep from the catcher
+
+    Returns:
+    - target_pos: np.array, target position for the thrower robot
+    """
+
+    thrower_pos = np.array(thrower_pos)
+    catcher_pos = np.array(catcher_pos)
+
+    # Calculate the direction vector from catcher to thrower
+    direction = thrower_pos - catcher_pos
+    distance = np.linalg.norm(direction)
+
+    # Normalize the direction vector
+    unit_direction = direction / distance
+
+    # Set the target position 2.5m away from the catcher along the line
+    max_distance = clearance_dist
+    target_pos = catcher_pos + unit_direction * max_distance
+
+    # Set the z-coordinate to 0.2 for the target position
+    target_pos[2] = 0.2
+
+    return target_pos
+
+
+def move_robot_closer(thrower_fname, catcher_fname, waypoints, C: ry.Config, isRender:bool, clearance_dist=2.5, target_pos:np.array=None, debug=False):
+# def move_robot_closer(thrower_fname, catcher_fname, waypoints, C: ry.Config, isRender:bool, target_pos:np.array=np.array([2.5, 0, 0.2]), debug=False):
     """
     Function to move the robot closer to the bin/catcher bot.
 
@@ -16,17 +50,23 @@ def move_robot_closer(thrower_fname, catcher_fname, waypoints, C: ry.Config, isR
     - waypoints: list, list of waypoints' frame names
     - C: ry.Config, configuration of the robot
     - isRender: bool, flag to render the RRT simulation of the robot moving closer
+    - clearance_dist: float, distance to keep from the catcher
+    - target_pos: np.array, target position for the thrower robot
+    - debug: bool, flag to print debug information
     """
 
     q0 = C.getJointState()
-
     C.getFrame(thrower_fname).setParent(C.getFrame("thrower_base"), True, False)
+    if target_pos is None:
+        target_pos = calc_target_pos(C.getFrame(thrower_fname).getPosition(), C.getFrame(catcher_fname).getPosition(), clearance_dist=clearance_dist)
 
     # define target location, i.e. a point [2.5, 0, 0.2] away from the bin/catcher
-    target_pos = np.array([2.5, 0, 0.2])
-    target_marker = C.addFrame("target_marker", catcher_fname)
-    target_marker.setShape(ry.ST.marker, [1])
-    target_marker.setRelativePosition(target_pos)
+    target_fname = "target_marker"
+    target_marker = C.addFrame(target_fname, catcher_fname) \
+        .setShape(ry.ST.marker, [1]) \
+        .setColor([1, 0, 0]) \
+        .setPosition(target_pos) \
+    
     target_marker.unLink()
 
     ## First solve KOMO problem
@@ -41,9 +81,25 @@ def move_robot_closer(thrower_fname, catcher_fname, waypoints, C: ry.Config, isR
     komo_1.addObjective(
         times=[],
         feature=ry.FS.positionDiff,
-        frames=[thrower_fname, "target_marker"],
+        frames=[thrower_fname, target_fname],
         type=ry.OT.eq,
         scale=[1e1],
+    )
+
+    komo_1.addObjective(
+        times=[],
+        feature=ry.FS.vectorXDiff,
+        frames=[thrower_fname, target_fname],
+        type=ry.OT.eq,
+        scale=[1e2],
+    )
+
+    komo_1.addObjective(
+        times=[],
+        feature=ry.FS.vectorYDiff,
+        frames=[thrower_fname, target_fname],
+        type=ry.OT.eq,
+        scale=[1e2],
     )
 
 
@@ -51,10 +107,11 @@ def move_robot_closer(thrower_fname, catcher_fname, waypoints, C: ry.Config, isR
     ret_1 = ry.NLP_Solver(komo_1.nlp(), verbose= 1) .solve()
     thrower_to_catcher = komo_1.getPath()
 
-    if ret_1.feasible:
-        print("KOMO_1 is feasible")
-    else:
-        print("KOMO_1 is NOT feasible")
+    if debug:
+        if ret_1.feasible:
+            print("KOMO_1 is feasible")
+        else:
+            print("KOMO_1 is NOT feasible")
 
     # Now solve RRT problem
     ry.params_clear()
@@ -71,27 +128,31 @@ def move_robot_closer(thrower_fname, catcher_fname, waypoints, C: ry.Config, isR
         foundRRTPath = ret_RRT_1.feasible
         numOfTrials = numOfTrials + 1
 
-    if foundRRTPath:
-        print("*** Found path at trial num #", numOfTrials, " ***")
-    else:
-        print("** No path found ***")
+    if debug:
+        if foundRRTPath:
+            print("*** Found path at trial num #", numOfTrials, " ***")
+        else:
+            print("** No path found ***")
 
     if isRender:
         for t in range(0, path_1.shape[0]-1):
             C.setJointState(path_1[t])
             C.view()
-            time.sleep(0.25)
+            time.sleep(0.01)
     # cleanup
-    C.delFrame("target_marker")
+    C.delFrame(target_fname)
+    C.getFrame(thrower_fname).unLink()
+    del komo_1
 
-def check_if_throw_is_obstructed(thrower_fname, catcher_fname, C):
+def check_if_throw_is_obstructed(thrower_fname, catcher_fname, C, threshold=0.5):
     """
     Function to check if the throw is obstructed by any obstacle.
 
     Args:
     - thrower_fname: string, frame name of the thrower robot
-    - catcher_fname: string, frame name of the catcher robot
+    - catcher_fname: string, frame name of the catcher robot/bin
     - C: ry.Config, configuration of the robot
+    - threshold: float, distance threshold to check if the throw is obstructed
 
     Returns:
     - isObstructed: bool, flag to indicate if the throw is obstructed
@@ -109,12 +170,7 @@ def check_if_throw_is_obstructed(thrower_fname, catcher_fname, C):
     dist = np.linalg.norm(thrower_pos - catcher_pos)
 
     # if the distance is less than a threshold, the throw is obstructed
-    if dist > 1.5:
-        isObstructed = True
-    else:
-        isObstructed = False
-
-    return isObstructed
+    return dist >= threshold
 
 
 # Main Function only for demonstration purposes
@@ -139,7 +195,7 @@ if __name__ == "__main__":
     thrower.setPosition(initial_thrower_pos + np.array([-2.5, 0, 0]))
 
     wall = C.addFrame(name="wall", parent="base") \
-        .setPosition(initial_thrower_pos + np.array([-1, 1, 0.8])) \
+        .setPosition(initial_thrower_pos + np.array([-1, 0.75, 0.8])) \
         .setShape(ry.ST.ssBox, [0.1, 2, 2, 0.02]) \
         .setColor([0, 1, 0]) \
         .setContact(1) \
@@ -154,12 +210,11 @@ if __name__ == "__main__":
 
 
     # TODO(Shayan): Add waypoints    
-
     isObstructed = check_if_throw_is_obstructed(thrower_fname, catcher_fname, C)
     print("Is throw obstructed:", isObstructed)
 
-    C.view()
-    time.sleep(5)
+    # C.view()
+    # time.sleep(5)
 
     if isObstructed:
         move_robot_closer(thrower_fname, catcher_fname, ["wall"], C, isRender=True)
